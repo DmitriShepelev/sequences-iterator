2c4202c08684c1ff643f339418dab0f4eb655585
dmitri_shepelev <shepelev.dm@gmail.com>
2021-09-21 21:17:19 +0300

    Added implementation of SequenceGenerator

--- SequencesTask/SequenceGenerator.cs
+++ SequencesTask/SequenceGenerator.cs
@@ -18,9 +18,29 @@ namespace SequencesTask
         /// <exception cref="ArgumentException">Thrown when <paramref name="count"/> is less than 1.</exception>
         public static IEnumerable<BigInteger> GetFibonacciNumbers(int count)
         {
-            throw new NotImplementedException();
+            if (count < 1)
+            {
+                throw new ArgumentException("Count of elements cannot be less than one.");
+            }
+            return Fibonacci(count);
+
+            static IEnumerable<BigInteger> Fibonacci(int count)
+            {
+                BigInteger prev = 0;
+                BigInteger curr = 1;
+                for (int i = 0; i < count; i++)
+                {
+                    yield return prev;
+                    BigInteger tmp = prev + curr;
+                    prev = curr;
+                    curr = tmp;
+                }
+            }
+
         }
-        
+
+
+
         /// <summary>
         /// Generates the sequence of prime numbers.
         /// </summary>
@@ -29,7 +49,35 @@ namespace SequencesTask
         /// <exception cref="ArgumentException">Thrown when <paramref name="count"/> is less than 1.</exception>
         public static IEnumerable<int> GetPrimeNumbers(int count)
         {
-            throw new NotImplementedException();
+            if (count < 1)
+            {
+                throw new ArgumentException("Count of elements cannot be less than one.");
+            }
+            return Prime(count);
+
+            static IEnumerable<int> Prime(int count)
+            {
+                int found = 0;
+                for (int i = 2; found < count; i++)
+                {
+                    int limit = (int)Math.Sqrt(i);
+                    bool IsPrime = true;
+                    for (int j = 2; j <= limit; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            IsPrime = false;
+                            break;
+                        }
+                    }
+                    if (IsPrime)
+                    {
+                        yield return i;
+                        found++;
+                    }
+                }
+
+            }
         }
 
     }
